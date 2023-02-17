9de2f4778e6582dedefdf1aab028515526c779af
Jawad <jawad@Jawads-Air.fritz.box>
2023-02-17 15:52:29 +0100

    Student Class

---
+++ src/main/java/com/example/demo/student/Student.java
@@ -0,0 +1,81 @@
+package com.example.demo.student;
+
+import java.time.LocalDate;
+
+public class Student {
+    private Long id;
+    private String name;
+    private Integer email;
+    private LocalDate date;
+    private Integer age;
+
+    public Student() {
+    }
+
+    public Student(Long id, String name, Integer email, LocalDate date, Integer age) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.date = date;
+        this.age = age;
+    }
+
+    public Student(String name, Integer email, LocalDate date, Integer age) {
+
+        this.name = name;
+        this.email = email;
+        this.date = date;
+        this.age = age;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getEmail() {
+        return email;
+    }
+
+    public void setEmail(Integer email) {
+        this.email = email;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email=" + email +
+                ", date=" + date +
+                ", age=" + age +
+                '}';
+    }
+}
